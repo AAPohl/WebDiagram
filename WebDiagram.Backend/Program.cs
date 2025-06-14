2400ec015da0a8c07d5942c1c17ad0249baab819
AAPohl <me@privacy.net>
2025-06-14 23:08:33 +0200

    Margin from backend

--- WebDiagram.Backend/Program.cs
+++ WebDiagram.Backend/Program.cs
@@ -12,5 +12,16 @@ app.MapGet("/render", ([FromQuery] float xMin, [FromQuery] float xMax, [FromQuer
     var imgBytes = Renderer.RenderCameraView(xMin, xMax, yMin, yMax);
     return Results.File(imgBytes, "image/png");
 });
+app.MapGet("/config", () =>
+{
+    var margin = new {
+        top = 40,
+        bottom = 40,
+        left = 40,
+        right = 40
+    };
+
+    return Results.Json(new { margin });
+});
 
 app.Run();

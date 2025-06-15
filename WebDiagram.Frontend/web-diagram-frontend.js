534f370c0ea0b8d7cfb1314b60718f1929048a6c
AAPohl <me@privacy.net>
2025-06-15 11:55:53 +0200

    img in component

--- WebDiagram.Frontend/web-diagram-frontend.js
+++ WebDiagram.Frontend/web-diagram-frontend.js
@@ -1,5 +1,14 @@
 class WebDiagramFrontend extends HTMLElement {
     connectedCallback() {
+        // Template erzeugen
+        const img = document.createElement('img');
+        img.id = "view";
+        img.width = this.getAttribute("width") || 100;
+        img.height = this.getAttribute("height") || 100;
+        img.style.border = "2px solid #555";
+        img.style.background = "black";
+
+        this.appendChild(img);
         alert("Hello from WebDiagramFrontend!");
     }
   }

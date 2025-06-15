89253c892f769903764b7839737be9a3de887473
AAPohl <me@privacy.net>
2025-06-15 16:41:49 +0200

    mouse wheel to component

--- WebDiagram.Frontend/web-diagram-frontend.js
+++ WebDiagram.Frontend/web-diagram-frontend.js
@@ -1,14 +1,13 @@
 class WebDiagramFrontend extends HTMLElement {
     connectedCallback() {
-        const img = document.createElement('img');
-        img.id = "view";
-        img.width = this.getAttribute("width") || 100;
-        img.height = this.getAttribute("height") || 100;
-        img.style.border = "2px solid #555";
-        img.style.background = "black";
+        this.img = document.createElement('img');
+        this.img.id = "view";
+        this.img.width = this.getAttribute("width") || 100;
+        this.img.height = this.getAttribute("height") || 100;
+        this.img.style.border = "2px solid #555";
+        this.img.style.background = "black";
 
-        this.appendChild(img);
-        this.img = img;
+        this.appendChild(this.img);
         this.source = this.getAttribute("source")
 
         fetch(`${this.source}/config`)
@@ -23,11 +22,13 @@ class WebDiagramFrontend extends HTMLElement {
 
         this.stepX = 0.02;
         this.stepY = 0.02;
+        this.zoomFactor = 1.1;
 
-        img.addEventListener("mousedown", this.startDrag.bind(this));
+        this.img.addEventListener("mousedown", this.startDrag.bind(this));
         window.addEventListener("mouseup", this.stopDrag.bind(this));
         window.addEventListener("mousemove", this.dragMove.bind(this));
         window.addEventListener("keydown", this.handleKeyDown.bind(this));
+        this.img.addEventListener("wheel", this.handleWheel.bind(this));
     }
 
     setInitialViewPort(viewPort) {
@@ -100,7 +101,60 @@ class WebDiagramFrontend extends HTMLElement {
     
         this.updateImage(this.viewPort);
     }
+
+    handleWheel(e) {
+        const rect = this.img.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+        const width = this.viewPort.xMax - this.viewPort.xMin;
+        const height = this.viewPort.yMax - this.viewPort.yMin;
     
+        // Zoom-Maßstab
+        const scale = (e.deltaY < 0) ? (1 / this.zoomFactor) : this.zoomFactor;
+    
+        // Zoom unten (untere Margin)
+        if (mouseY >= rect.height - this.margin.bottom) {
+          const worldX = this.viewPort.xMin + ((mouseX - this.margin.left) / (rect.width - this.margin.left - this.margin.right)) * width;
+          const newWidth = width * scale;
+          const newXMin = worldX - ((mouseX - this.margin.left) / (rect.width - this.margin.left - this.margin.right)) * newWidth;
+          this.viewPort.xMin = newXMin;
+          this.viewPort.xMax = newXMin + newWidth;
+          this.updateImage(this.viewPort);
+          e.preventDefault();
+          return;
+        }
+    
+        // Zoom links (linke Margin)
+        if (mouseX <= this.margin.left) {
+          const worldY = this.viewPort.yMax - ((mouseY - this.margin.top) / (rect.height - this.margin.top - this.margin.bottom)) * height;
+          const newHeight = height * scale;
+          const newYMax = worldY + ((mouseY - this.margin.top) / (rect.height - this.margin.top - this.margin.bottom)) * newHeight;
+          this.viewPort.yMin = newYMax - newHeight;
+          this.viewPort.yMax = newYMax;
+          this.updateImage(this.viewPort);
+          e.preventDefault();
+          return;
+        }
+    
+        // Zoom innen (mittlerer Bereich)
+        const insideHorizontal = mouseX > this.margin.left && mouseX < (rect.width - this.margin.right);
+        const insideVertical = mouseY > this.margin.top && mouseY < (rect.height - this.margin.bottom);
+    
+        if (insideHorizontal && insideVertical) {
+          const worldX = this.viewPort.xMin + ((mouseX - this.margin.left) / (rect.width - this.margin.left - this.margin.right)) * width;
+          const worldY = this.viewPort.yMax - ((mouseY - this.margin.top) / (rect.height - this.margin.top - this.margin.bottom)) * height;
+          const newWidth = width * scale;
+          const newHeight = height * scale;
+          const newXMin = worldX - ((mouseX - this.margin.left) / (rect.width - this.margin.left - this.margin.right)) * newWidth;
+          const newYMax = worldY + ((mouseY - this.margin.top) / (rect.height - this.margin.top - this.margin.bottom)) * newHeight;
+          this.viewPort.xMin = newXMin;
+          this.viewPort.xMax = newXMin + newWidth;
+          this.viewPort.yMin = newYMax - newHeight;
+          this.viewPort.yMax = newYMax;
+          this.updateImage(this.viewPort);
+          e.preventDefault();
+        }
+    }    
 }
   
 customElements.define('web-diagram-frontend', WebDiagramFrontend);

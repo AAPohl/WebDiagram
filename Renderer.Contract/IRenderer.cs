ed3e6f01a44085a56adb3a772f6a5ddeaa84d55d
AAPohl <82733706+AAPohl@users.noreply.github.com>
2025-06-20 12:40:42 +0200

    Update IRenderer.cs

--- Renderer.Contract/IRenderer.cs
+++ Renderer.Contract/IRenderer.cs
@@ -2,6 +2,7 @@ namespace Renderer.Contract
 {
 	public interface IRenderer
 	{
+		Margin Margin { get; set; }
 		byte[] Render(float xMin, float xMax, float yMin, float yMax, int width, int height);
 	}
 }

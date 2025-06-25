a5bfd308990f09a44a5a849ba2a298be1efe0cbb
AAPohl <me@privacy.net>
2025-06-25 21:52:20 +0200

    Backend Kapselung: DiagramManager

--- WebDiagram.App/Program.cs
+++ WebDiagram.App/Program.cs
@@ -18,65 +18,11 @@ app.MapGet("/", context =>
     return Task.CompletedTask;
 });
 
-app.MapHub<DiagramHub>("/diagramhub");
-var hubContext = app.Services.GetRequiredService<IHubContext<DiagramHub>>();
 
-var diagramManager = new DiagramManager(hubContext);
-diagramManager.AddRenderer("diagram1", renderAction => new SkiaSharpRenderer(24, renderAction));
-diagramManager.AddRenderer("diagram2", renderAction => new SkiaSharpRenderer(24, renderAction));
-app.MapGet("/{instanceId}/updateSize", (
-	string instanceId,
-	[FromQuery] int width,
-	[FromQuery] int height) =>
-{
-	try
-	{
-        diagramManager.GetRenderer(instanceId).UpdateSize(width, height);
-		return Results.Ok();
-	}
-	catch
-	{
-        return Results.Problem();
-	}
-});
 
-app.MapGet("/{instanceId}/updateViewPort", (
-	string instanceId,
-    [FromQuery] float xMin,
-	[FromQuery] float xMax,
-	[FromQuery] float yMin,
-	[FromQuery] float yMax) =>
-{
-	try
-	{
-		diagramManager.GetRenderer(instanceId).UpdateViewport(xMin, xMax, yMin, yMax);
-		return Results.Ok();
-	}
-	catch
-	{
-		return Results.Problem();
-	}
-});
-
-app.MapGet("/{instanceId}/config", (
-    string instanceId) =>
-{
-    var margin = new
-    {
-        top = diagramManager.GetRenderer(instanceId).Margin.Top,
-        bottom = diagramManager.GetRenderer(instanceId).Margin.Bottom,
-        left = diagramManager.GetRenderer(instanceId).Margin.Left,
-        right = diagramManager.GetRenderer(instanceId).Margin.Right
-    };
-
-    return Results.Json(new { margin });
-});
-
-diagramManager.Start();
-
-app.Lifetime.ApplicationStopping.Register(() =>
-{
-	diagramManager.Stop();
-});
+var webDiagramBackend = new WebDiagramBackend(app);
+webDiagramBackend.AddRenderer("diagram1", renderAction => new SkiaSharpRenderer(24, renderAction));
+webDiagramBackend.AddRenderer("diagram2", renderAction => new SkiaSharpRenderer(24, renderAction));
+webDiagramBackend.Start();
 
 app.Run();
--- WebDiagram.Backend/DiagramManager.cs
+++ WebDiagram.Backend/DiagramManager.cs
@@ -1,14 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Renderer.Contract;
 
-public class DiagramManager
+public class WebDiagramBackend
 {
-    private readonly Dictionary<string, IRenderer> renderer;
+    private readonly Dictionary<string, IRenderer> renderer = new Dictionary<string, IRenderer>();
     private readonly IHubContext<DiagramHub> hubContext;
-    public DiagramManager(IHubContext<DiagramHub> hubContext)
+    private readonly WebApplication webApplication;
+    public WebDiagramBackend(WebApplication webApplication)
     {
-        this.hubContext = hubContext;
-        renderer = new Dictionary<string, IRenderer>();
+        this.webApplication = webApplication;
+        hubContext = webApplication.Services.GetRequiredService<IHubContext<DiagramHub>>();
     }
 
     public void AddRenderer(string instanceId, Func<Action<byte[]>, IRenderer> renderFactory) => renderer.Add(instanceId, renderFactory(image => hubContext.Clients.Group(instanceId).SendAsync("ReceiveImage", Convert.ToBase64String(image)).GetAwaiter().GetResult()));
@@ -17,14 +19,64 @@ public class DiagramManager
 
     public void Start()
     {
+        webApplication.MapHub<DiagramHub>("/diagramhub");
+        webApplication.MapGet("/{instanceId}/updateSize", (
+            string instanceId,
+            [FromQuery] int width,
+            [FromQuery] int height) =>
+        {
+            try
+            {
+                renderer[instanceId].UpdateSize(width, height);
+                return Results.Ok();
+            }
+            catch
+            {
+                return Results.Problem();
+            }
+        });
+
+        webApplication.MapGet("/{instanceId}/updateViewPort", (
+            string instanceId,
+            [FromQuery] float xMin,
+            [FromQuery] float xMax,
+            [FromQuery] float yMin,
+            [FromQuery] float yMax) =>
+        {
+            try
+            {
+                renderer[instanceId].UpdateViewport(xMin, xMax, yMin, yMax);
+                return Results.Ok();
+            }
+            catch
+            {
+                return Results.Problem();
+            }
+        });
+
+        webApplication.MapGet("/{instanceId}/config", (
+            string instanceId) =>
+        {
+            var margin = new
+            {
+                top = renderer[instanceId].Margin.Top,
+                bottom = renderer[instanceId].Margin.Bottom,
+                left = renderer[instanceId].Margin.Left,
+                right = renderer[instanceId].Margin.Right
+            };
+
+            return Results.Json(new { margin });
+        });
+
         foreach (var renderer in renderer)
             renderer.Value.Start();
-    }
-    
-    public void Stop()
-    {
-        foreach(var renderer in renderer)
+
+        webApplication.Lifetime.ApplicationStopping.Register(() =>
+        {
+            foreach(var renderer in renderer)
 	        renderer.Value.Stop();
+        });
+        
     }
 
 }
